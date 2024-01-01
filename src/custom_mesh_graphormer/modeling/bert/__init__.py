5417f14e154fef929505c41f54d9be2b7e127c8c
tai17082006.tk@gmail.com <tai17082006.tk@gmail.com>
2024-01-01 15:59:32 +0700

    sync

--- src/custom_mesh_graphormer/modeling/bert/__init__.py
+++ src/custom_mesh_graphormer/modeling/bert/__init__.py
@@ -8,8 +8,8 @@ from .modeling_graphormer import Graphormer
 from .e2e_body_network import Graphormer_Body_Network
 
 from .e2e_hand_network import Graphormer_Hand_Network
-
-from .modeling_utils import (WEIGHTS_NAME, CONFIG_NAME, TF_WEIGHTS_NAME,
+CONFIG_NAME = "config.json"
+from .modeling_utils import (WEIGHTS_NAME, TF_WEIGHTS_NAME,
                           PretrainedConfig, PreTrainedModel, prune_layer, Conv1D)
 
 from .file_utils import (PYTORCH_PRETRAINED_BERT_CACHE)
